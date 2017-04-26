b5b7619e2ccf46837a00d34b71fc0d1aeed4f674
Sidney Nguyen <sidneynguyendev@gmail.com>
2017-04-25 23:17:52 -0700

    Implemented Facebook login functionality

--- app/src/main/java/com/mobewash/mobewash/LoginFragment.java
+++ app/src/main/java/com/mobewash/mobewash/LoginFragment.java
@@ -1,11 +1,19 @@
 package com.mobewash.mobewash;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 /**
@@ -13,17 +21,48 @@ import android.view.ViewGroup;
  */
 public class LoginFragment extends Fragment {
 
+    private CallbackManager mCallbackManager;
+    private LoginButton mLoginButton;
 
     public LoginFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCallbackManager = CallbackManager.Factory.create();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        mLoginButton = (LoginButton) view.findViewById(R.id.fb_login_button);
+        mLoginButton.setReadPermissions("email");
+        mLoginButton.setFragment(LoginFragment.this);
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+        return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
