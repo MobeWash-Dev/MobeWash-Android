f7233f23394777cfabb3af752ed2ebeb6a73513b
Sidney Nguyen <sidneynguyendev@gmail.com>
2017-04-27 23:00:14 -0700

    Implemented simple navigation between login/sign up pages

--- app/src/main/java/com/mobewash/mobewash/LoginActivity.java
+++ app/src/main/java/com/mobewash/mobewash/LoginActivity.java
@@ -1,13 +1,55 @@
 package com.mobewash.mobewash;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.facebook.login.LoginResult;
+
+public class LoginActivity extends AppCompatActivity implements LoginFragment.OnLoginFragmentInteractionListener, FragmentManager.OnBackStackChangedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
+        LoginFragment loginFragment = new LoginFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.framelayout_fragment_container, loginFragment).commit();
+    }
+
+    @Override
+    public void onSignUpButtonClick() {
+        SignupFragment signupFragment = new SignupFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.framelayout_fragment_container, signupFragment).addToBackStack("signup").commit();
+    }
+
+    @Override
+    public void onLoginButtonClick() {
+        EmailLoginFragment emailLoginFragment = new EmailLoginFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.framelayout_fragment_container, emailLoginFragment).addToBackStack("emaillogin").commit();
+    }
+
+    @Override
+    public void onFacebookLogin(LoginResult loginResult) {
+
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        LoginFragment loginFragment = new LoginFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.framelayout_fragment_container, loginFragment).commit();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        return true;
     }
 }
--- app/src/main/java/com/mobewash/mobewash/LoginFragment.java
+++ app/src/main/java/com/mobewash/mobewash/LoginFragment.java
@@ -1,6 +1,7 @@
 package com.mobewash.mobewash;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -21,8 +23,22 @@ import com.facebook.login.widget.LoginButton;
  */
 public class LoginFragment extends Fragment {
 
+    //
+    // Facebook
+    //
     private CallbackManager mCallbackManager;
-    private LoginButton mLoginButton;
+    private LoginButton mFBLoginButton;
+
+    //
+    // Email
+    //
+    private Button mLoginButton;
+    private Button mSignUpButton;
+
+    //
+    // Member Variables
+    //
+    private OnLoginFragmentInteractionListener mListener;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -38,13 +54,15 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
-        mLoginButton = (LoginButton) view.findViewById(R.id.fb_login_button);
-        mLoginButton.setReadPermissions("email");
-        mLoginButton.setFragment(LoginFragment.this);
-        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+
+        // Setup FB login button
+        mFBLoginButton = (LoginButton) view.findViewById(R.id.fb_login_button);
+        mFBLoginButton.setReadPermissions("email");
+        mFBLoginButton.setFragment(LoginFragment.this);
+        mFBLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-
+                mListener.onFacebookLogin(loginResult);
             }
 
             @Override
@@ -57,6 +75,22 @@ public class LoginFragment extends Fragment {
 
             }
         });
+
+        //Setup email login/signup buttons
+        mLoginButton = (Button) view.findViewById(R.id.button_login);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onLoginButtonClick();
+            }
+        });
+        mSignUpButton = (Button) view.findViewById(R.id.button_signup);
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onSignUpButtonClick();
+            }
+        });
         return view;
     }
 
@@ -64,5 +98,28 @@ public class LoginFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnLoginFragmentInteractionListener) {
+            mListener = (OnLoginFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnLoginFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnLoginFragmentInteractionListener {
+        void onSignUpButtonClick();
+        void onLoginButtonClick();
+        void onFacebookLogin(LoginResult loginResult);
     }
 }
