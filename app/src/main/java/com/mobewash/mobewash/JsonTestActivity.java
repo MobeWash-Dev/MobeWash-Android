53bf4fea725a73f0947967c7648816de80ff2a15
Sidney Nguyen <sidneynguyendev@gmail.com>
2017-05-14 01:11:04 -0700

    Created a RestRequester object

---
+++ app/src/main/java/com/mobewash/mobewash/JsonTestActivity.java
@@ -0,0 +1,22 @@
+package com.mobewash.mobewash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.json.JSONObject;
+
+public class JsonTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json_test);
+        RestRequester restRequester = new RestRequester(JsonTestActivity.this);
+        restRequester.get("http://10.0.2.2:3000/", new RestRequester.OnRequestCompleteListener() {
+            @Override
+            public void onRequestComplete(Exception err, JSONObject jsonObject) {
+                
+            }
+        });
+    }
+}
