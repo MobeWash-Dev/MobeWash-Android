dc68fb7825224d1f3cb7f2bc5b570043f600d03c
skapil862 <skapil@ucsd.edu>
2017-05-21 04:42:38 -0700

    DateActivity2 Holds widget Calendar instead of Dialog
    Summary says it all.

---
+++ app/src/main/java/com/mobewash/mobewash/DateTimeActivity2.java
@@ -0,0 +1,163 @@
+package com.mobewash.mobewash;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTimeActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_time2);
+
+
+        final CaldroidFragment caldroidFragment = new CaldroidFragment();
+        final Bundle args = new Bundle();
+        args.putBoolean(CaldroidFragment.ENABLE_SWIPE, true);
+        caldroidFragment.setArguments(args);
+
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+        int[] enabledDays = {2,3};
+        ArrayList<String> disabledDates = addDisabledDates(enabledDays);
+        caldroidFragment.setDisableDatesFromString(disabledDates);
+        caldroidFragment.setMinDate(Calendar.getInstance().getTime());
+
+        ArrayList<Date> disabledDates2 = addDisabledDates2(enabledDays);
+        ColorDrawable white = new ColorDrawable(Color.WHITE);
+        for (Date date : disabledDates2)
+          caldroidFragment.setBackgroundDrawableForDate(white,date);
+
+        final CaldroidListener listener = new CaldroidListener() {
+
+            @Override
+            public void onSelectDate(Date date, View view) {
+                Toast.makeText(getApplicationContext(), date.toString().substring(0,10),
+                        Toast.LENGTH_SHORT).show();
+                caldroidFragment.clearSelectedDates();
+                caldroidFragment.moveToDate(date);
+                caldroidFragment.setCalendarDate(date);
+                caldroidFragment.setSelectedDate(date);
+                caldroidFragment.refreshView();
+            }
+
+            @Override
+            public void onChangeMonth(int month, int year) {
+            }
+
+            @Override
+            public void onLongClickDate(Date date, View view) {
+            }
+
+            @Override
+            public void onCaldroidViewCreated() {
+            }
+
+        };
+
+        caldroidFragment.setCaldroidListener(listener);
+
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.calendar1, caldroidFragment);
+        t.commit();
+
+
+
+    }
+
+
+    private ArrayList<String> addDisabledDates(int[] enabledDays) {
+
+        //our format of JSON file has 0-6 for Monday to Sunday which isn't desirable
+        ArrayList<String> disabledDates = new ArrayList<String>();
+        boolean foundFirstDate = false;
+        Calendar firstDate = Calendar.getInstance();
+        boolean[] formattedEnabledDays = new boolean[7];
+        for(int i = 0; i < enabledDays.length ; i++) {
+            if(enabledDays[i] == 6)
+                formattedEnabledDays[0] = true;
+            else
+                formattedEnabledDays[enabledDays[i]+1] = true;
+        }//set formattedEnabledDays to be true
+        for(int i = 0; i < 7; i++) {
+            Log.d("myTag","" + i + " " + formattedEnabledDays[i]);
+        }
+        for(int i = 0; i < 7; i++) {
+            Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.DATE,i);
+            if(!formattedEnabledDays[cal.get(Calendar.DAY_OF_WEEK)-1]) {
+                if(!foundFirstDate) {
+                    foundFirstDate = true;
+                    firstDate = cal;
+                }
+                Calendar calToAdd = Calendar.getInstance();
+                calToAdd.add(Calendar.DATE,i-7);
+                for (int j = 0; j < 100; j++) {
+                    calToAdd.add(Calendar.DATE,7);
+                    String stringToAdd = "" + calToAdd.get(Calendar.YEAR) + "-" +
+                            (calToAdd.get(Calendar.MONTH) + 1) + "-" +
+                            calToAdd.get(Calendar.DATE);
+                    disabledDates.add(stringToAdd);
+                }
+            }
+        }
+        return disabledDates;
+    }
+
+    private ArrayList<Date> addDisabledDates2(int[] enabledDays) {
+        //our format of JSON file has 0-6 for Monday to Sunday which isn't desirable
+        ArrayList<Date> disabledDates = new ArrayList<Date>();
+        boolean foundFirstDate = false;
+        Calendar firstDate = Calendar.getInstance();
+        boolean[] formattedEnabledDays = new boolean[7];
+        for(int i = 0; i < enabledDays.length ; i++) {
+            if(enabledDays[i] == 6)
+                formattedEnabledDays[0] = true;
+            else
+                formattedEnabledDays[enabledDays[i]+1] = true;
+        }//set formattedEnabledDays to be true
+        for(int i = 0; i < 7; i++) {
+            Log.d("myTag","" + i + " " + formattedEnabledDays[i]);
+        }
+        for(int i = 0; i < 7; i++) {
+            Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.DATE,i);
+            if(!formattedEnabledDays[cal.get(Calendar.DAY_OF_WEEK)-1]) {
+                if(!foundFirstDate) {
+                    foundFirstDate = true;
+                    firstDate = cal;
+                }
+                Calendar calToAdd = Calendar.getInstance();
+                calToAdd.add(Calendar.DATE,i-7);
+                for (int j = 0; j < 100; j++) {
+                    calToAdd.add(Calendar.DATE,7);
+                    Date dateToAdd = calToAdd.getTime();
+                    disabledDates.add(dateToAdd);
+                }
+            }
+        }
+        return disabledDates;
+    }
+
+
+
+}
