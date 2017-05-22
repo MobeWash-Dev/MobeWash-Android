bc997ae2d1d2e32a6a93c954cfa7e0030d1d2a42
Danny <7777777707a@gmail.com>
2017-05-21 17:22:38 -0700

    company page

---
+++ app/src/main/java/com/mobewash/mobewash/CompanyFragment.java
@@ -0,0 +1,145 @@
+package com.mobewash.mobewash;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.mobewash.mobewash.models.CompanyData;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CompanyFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link CompanyFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CompanyFragment extends Fragment {
+
+    private ListView listView;
+
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public CompanyFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CompanyFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CompanyFragment newInstance(String param1, String param2) {
+        CompanyFragment fragment = new CompanyFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_company, container, false);
+        listView = (ListView) view.findViewById(R.id.List);
+
+        RestRequester myReq = new RestRequester(this.getContext());
+        myReq.getArray("https://mobe-server.herokuapp.com/api/company", new RestRequester.OnArrayRequestCompleteListener() {
+            @Override
+            public void onArrayRequestComplete(Exception err, JSONArray jsonArray) {
+                if(err == null) {
+                    ArrayList<CompanyData> data = JSONParser.parseCompanyData(jsonArray);
+                    ArrayList<String> names = new ArrayList<String>();
+                    for( int i = 0; i < data.size(); i++){
+                        names.add(data.get(i).getName());
+                    }
+                    String [] items = names.toArray(new String[names.size()]);
+
+                    ArrayAdapter<String> itemsAdapter =
+                            new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1,items);
+                    listView.setAdapter(itemsAdapter);
+                }
+            }
+
+        });
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
