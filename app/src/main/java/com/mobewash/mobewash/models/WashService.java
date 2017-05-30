9f82b62b3ac7fbd37affb00764d6646a7e53c6d9
Sidney Nguyen <sidneynguyendev@gmail.com>
2017-05-29 20:40:18 -0700

    Fixed services page to correctly parse server data

--- app/src/main/java/com/mobewash/mobewash/models/WashService.java
+++ app/src/main/java/com/mobewash/mobewash/models/WashService.java
@@ -11,27 +11,27 @@ import java.util.ArrayList;
  */
 
 public class WashService {
-    private String serviceName;
-    private int paymentAmount;
-    private String duration;
-    private String description;
-    private String additionalService;
-    private String additionalPrice;
+    private int uid;
     private int appointmentTypeID;
+    private String name;
+    private int cost;
+    private int extraCost;
+    private int length;
+    private int extraLength;
+    private String[] info;
 
     public WashService(JSONObject jsonObject) throws JSONException {
-        this.serviceName = jsonObject.getString("serviceName");
-        this.duration = jsonObject.getString("duration");
-        this.paymentAmount = jsonObject.getInt("paymentAmount");
-        this.description = jsonObject.getString("description");
-        if (jsonObject.has("additionalService")) {
-            this.additionalService = jsonObject.getString("additionalService");
-        }
-        this.additionalPrice = jsonObject.getString("additionalPrice");
-        if (serviceName.equals("MobePlus")) {
-            this.appointmentTypeID = 2944155;
-        } else {
-            this.appointmentTypeID = 2925879;
+        this.uid = jsonObject.getInt("uid");
+        this.appointmentTypeID = jsonObject.getInt("appointmentTypeID");
+        this.name = jsonObject.getString("name");
+        this.cost = jsonObject.getInt("cost");
+        this.extraCost = jsonObject.getInt("extraCost");
+        this.length = jsonObject.getInt("length");
+        this.extraLength = jsonObject.getInt("extraLength");
+        JSONArray infoArray = jsonObject.getJSONArray("info");
+        this.info = new String[infoArray.length()];
+        for (int i = 0; i < infoArray.length(); i++) {
+            this.info[i] = infoArray.getString(i);
         }
     }
 
@@ -42,14 +42,20 @@ public class WashService {
 
     @Override
     public String toString() {
+        String infoString = "[" + getInfo()[0];
+        for (int i = 0; i < getInfo().length; i++) {
+            infoString += ", " + getInfo()[i];
+        }
+        infoString += "]";
         return "{\n"
-                + "serviceName: " + getServiceName() + "\n"
-                + "duration: " + getDuration() + "\n"
-                + "paymentAmount: " + getPaymentAmount() + "\n"
-                + "description: " + getDescription() + "\n"
-                + "additionalService: " + getAdditionalService() + "\n"
-                + "additionalPrice: " + getAdditionalPrice() + "\n"
-                + "appointmentTypeID: " + getAppointmentTypeID() + "}";
+                + "uid: " + getUid() + "\n"
+                + "appointmentTypeID: " + getAppointmentTypeID() + "\n"
+                + "name: " + getName() + "\n"
+                + "cost: " + getCost() + "\n"
+                + "extraCost: " + getExtraCost() + "\n"
+                + "length: " + getLength() + "\n"
+                + "extraLength: " + getExtraLength() + "\n"
+                + "info: " + infoString + "\n}";
     }
 
     public static ArrayList<WashService> parseWashServiceList(JSONArray jsonArray) {
@@ -65,32 +71,35 @@ public class WashService {
         return washServiceList;
     }
 
-    public String getServiceName() {
-        return serviceName;
-    }
-
-    public int getPaymentAmount() {
-        return paymentAmount;
-    }
-
-    public String getDuration() {
-        return duration;
-    }
-
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getAdditionalService() {
-        return additionalService;
-    }
-
-    public String getAdditionalPrice() {
-        return additionalPrice;
+    public int getUid() {
+        return uid;
     }
 
     public int getAppointmentTypeID() {
         return appointmentTypeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getExtraCost() {
+        return extraCost;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getExtraLength() {
+        return extraLength;
+    }
+
+    public String[] getInfo() {
+        return info;
     }
 }
